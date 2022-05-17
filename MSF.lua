16309ade1907097e6cab01e3a5fb67d2719f475f
nicelym <nicely.max@gmail.com>
2022-05-16 21:13:03 -0400

    Add header

---
+++ MSF.lua
@@ -0,0 +1,15 @@
+--[[
+@Module Mission Scripting Framework (MSF)
+
+@Author West#9009
+
+@Description
+Barebones mission scripting framework with modules to add functionality as needed for DCS
+mission development.
+
+@github: https://github.com/nicelym/MSF
+
+@Created 16MAY22
+
+@TODO
+]]
