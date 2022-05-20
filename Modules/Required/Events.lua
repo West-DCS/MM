dde228f856fa9e8dda260546ce9b29a3a1b71824
nicelym <nicely.max@gmail.com>
2022-05-20 00:41:43 -0400

    Added event instance.

--- Modules/Required/Events.lua
+++ Modules/Required/Events.lua
@@ -1,3 +1,11 @@
 ---@author West#9009
 ---@description Event dispatching.
 ---@created 20MAY22
+
+Events = {}
+
+function Events:New()
+    local self = BASE:Inherit(self, BASE:New())
+
+    return self
+end
