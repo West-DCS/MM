58fcfe0cd86975d0117fd700d00b7270a171d9a9
nicelym <nicely.max@gmail.com>
2022-05-20 00:47:08 -0400

    Added Events initialization.

--- Modules.lua
+++ Modules.lua
@@ -2,5 +2,6 @@
 dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Enums.lua')
 dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Routines.lua')
 dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Base.lua')
+dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Events.lua')
 
 -- Optional Modules, do touch. '--' To comment out and prevent running.
