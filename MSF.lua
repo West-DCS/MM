99d5a8715e173e0436c2734ed9fc394cebecbcc9
nicelym <nicely.max@gmail.com>
2022-05-20 01:14:40 -0400

    Initialized event dispatcher

--- MSF.lua
+++ MSF.lua
@@ -7,3 +7,6 @@ dofile(lfs.writedir() .. '/Scripts/MSF/Modules.lua')
 
 -- Log certifies that at least all modules loaded.
 BASE:Log('info', '%s initialization finished.', 'MSF')
+
+-- Load event dispatcher.
+__EVENTS = EVENTS:New()
