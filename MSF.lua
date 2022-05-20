9317af6e83b59ca7d4aa17a3653c1c4889a6e4b8
nicelym <nicely.max@gmail.com>
2022-05-20 00:30:44 -0400

    MSF start functions.

--- MSF.lua
+++ MSF.lua
@@ -1,15 +1,9 @@
---[[
-@Module Mission Scripting Framework (MSF)
+---@author West#9009
+---@description Mission Scripting Framework. Initialization functions.
+---@created 16MAY22
 
-@Author West#9009
+-- Load MSF Modules
+dofile(lfs.writedir() .. '/Scripts/MSF/Modules.lua')
 
-@Description
-Barebones mission scripting framework with modules to add functionality as needed for DCS
-mission development.
-
-@github: https://github.com/nicelym/MSF
-
-@Created 16MAY22
-
-@TODO
-]]
+-- Log certifies that at least all modules loaded.
+BASE:Log('info', '%s initialization finished.', 'MSF')
