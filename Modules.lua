93f041a31e64be02688cec1d5dc3f7bc94e1d8ab
nicelym <nicely.max@gmail.com>
2022-05-20 00:30:06 -0400

    Module initialization order added.

---
+++ Modules.lua
@@ -0,0 +1,6 @@
+-- Require Modules, do not touch. Order is important.
+dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Enums.lua')
+dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Routines.lua')
+dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Base.lua')
+
+-- Optional Modules, do touch. '--' To comment out and prevent running.
