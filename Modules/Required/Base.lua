fcbfa90e238f5e91a5c561d4b92fd54864ac9bab
nicelym <nicely.max@gmail.com>
2022-05-20 00:29:24 -0400

    Added initial BASE functions.

--- Modules/Required/Base.lua
+++ Modules/Required/Base.lua
@@ -1,12 +1,53 @@
---[[
-@Module Base
+---@author West#9009
+---@description Base object in which child objects are derived. Includes functions for logging.
+---@created 16MAY22
 
-@Author West#9009
+---@class BASE
+---@field public BASE table Public methods and fields.
+---@field public ClassName string The name of the class.
+---@field public self BASE Self reference.
+BASE = {
+    ClassName = 'BASE'
+}
 
-@Description
-Base object in which child objects are derived. Includes functions for logging.
+--- Initialize a new instance.
+---@return BASE Returns self.
+function BASE:New()
+    return ROUTINES.util.deepCopy(self)
+end
 
-@Created 16MAY22
+--- Inherit a parents fields and methods to a new child object.
+---@param Child table The object that inherits.
+---@param Parent table The object to inherit from.
+---@return BASE The new object with inherited methods.
+function BASE:Inherit(Child, Parent)
+    local Child = ROUTINES.util.deepCopy(Child)
 
-@TODO
-]]
+    setmetatable(Child, {__index = Parent})
+    return Child
+end
+
+---@return string The name of the class.
+function BASE:GetClassName()
+    return self.ClassName
+end
+
+--- Log a message to DCS.log
+---@param logType string info, warning, error
+---@param msg string The message to log.
+---@return BASE Returns self.
+function BASE:Log(logType, msg, ...)
+    local write = function(logType, ...)
+        logType(string.format('%s %s', self:GetClassName(), string.format(msg, ...)))
+    end
+
+    local logTypes = {
+        info = env.info,
+        warning = env.warning,
+        error = env.error
+    }
+
+    write(logTypes[logType], ...)
+
+    return self
+end
