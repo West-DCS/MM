7a9985c99c3b67c968dcbc3b0f2573d8ecd20576
nicelym <nicely.max@gmail.com>
2022-05-22 17:08:18 -0400

    Added new modules

--- Modules.lua
+++ Modules.lua
@@ -3,9 +3,16 @@ dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Enums.lua')
 dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Routines.lua')
 dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Base.lua')
 dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Events.lua')
+dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Database.lua')
+
+-- Wrapper Classes
+dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Object.lua')
+dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Unit.lua')
+dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Required/Group.lua')
 
 -- Instantiates event dispatcher here in case optional modules handle events.
 __EVENTS = EVENTS:New()
+__DATABASE = DATABASE:New()
 
 -- Optional and User Modules, do touch. '--' To comment out and prevent running.
 dofile(lfs.writedir() .. '/Scripts/MSF/Modules/Optional/Test.lua')
