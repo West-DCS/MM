522a8ba0248e8b0a691d8fec8121e002a126765d
nicelym <nicely.max@gmail.com>
2022-05-25 01:51:24 -0400

    SPAWN tests.

--- Modules/Optional/Test.lua
+++ Modules/Optional/Test.lua
@@ -2,8 +2,10 @@
 ---@description Test Module.
 ---@created 20MAY22
 
+---@type BASE
 TEST = {
-    ClassName = 'TEST'
+    ClassName = 'TEST',
+    iterations = 0
 }
 
 function TEST:New()
@@ -15,16 +17,20 @@ function TEST:New()
 end
 
 function TEST:_OnEventBirth(EventData)
-    local PlayerName = EventData.IniPlayerName
-    local zone = ZONE:FindByName('Larnaca Logistics Center')
-    local coords = zone:GetVec2()
 
-    self:Log('info', '%s coords x= %s, z = %s!', zone:GetName(), coords.x, coords.z)
+    if self.iterations == 0 then
+        self.iterations = self.iterations + 1
 
-    self:Schedule(10, function()
-        GROUP:FindByName('Aerial-1'):Destroy()
-        self:Log('info', 'Destroyed Unit!')
-    end)
+        local zone = ZONE:FindByName('Larnaca Logistics Center')
+        local farp = SPAWN:NewStaticFromType('Invisible FARP', 'Heliports', 80, nil,
+                nil, 'invisiblefarp', true)
+
+        farp:SpawnFromZone(zone)
+
+        self:Schedule(10, function() farp:Destroy() end)
+        self.iterations = self.iterations + 1
+    end
+
 end
 
 TEST:New()
