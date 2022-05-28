23ec384f62105efd58ea5eb3d285c54280664552
nicelym <nicely.max@gmail.com>
2022-05-28 04:13:23 -0400

    Added random, vec3, and illuminate to zone.

--- Modules/Required/Zone.lua
+++ Modules/Required/Zone.lua
@@ -35,6 +35,75 @@ function ZONE:GetVec2()
     return {x = self.x, z = self.z}
 end
 
+function ZONE:GetRandomVec2(SurfaceType)
+    local iterations = 0
+
+    local test = function()
+        local NotValid = false
+        local radius = self.Radius * math.sqrt(math.random())
+        local theta = math.random() * 2 * math.pi
+        local x = self.x + radius * math.cos(theta)
+        local z = self.z + radius * math.sin(theta)
+
+        if land.getSurfaceType({x = x, y = z}) == SurfaceType then
+
+            local Sphere =  {
+                id = world.VolumeType.SPHERE,
+                params = {
+                    point = {x = x, y = land.getHeight({x = x, y = z}), z = z},
+                    radius = 50
+                }
+            }
+
+            local search = function(object)
+                if object:isExist() then
+
+                    NotValid = true
+
+                    return false
+                end
+            end
+
+            world.searchObjects(Object.Category.SCENERY, Sphere, search)
+
+            if NotValid then
+                return nil
+            end
+
+            return {x = x, z = z}
+        else
+            return nil
+        end
+    end
+
+    while(iterations < 100) do
+        local result = test()
+
+        if result then
+            return result
+        end
+
+        iterations = iterations + 1
+    end
+end
+
+function ZONE:GetVec3()
+    local Vec3 = self:GetVec2()
+    Vec3.y = land.getHeight({x = Vec3.x, y = Vec3.z})
+
+    return Vec3
+end
+
+function ZONE:Illuminate()
+    local Vec3 = self:GetVec3()
+
+    Vec3.y = Vec3.y + 300
+
+    trigger.action.illuminationBomb(Vec3)
+
+    return self
+end
+
 function ZONE:GetName()
     return self.Name
 end
