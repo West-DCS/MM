d4c6f58855b4a9c9b4c04c814acc6fce2dbad27c
nicelym <nicely.max@gmail.com>
2022-06-09 03:08:23 -0400

    Updated add and update functions.

--- MSF.lua
+++ MSF.lua
@@ -3,12 +3,10 @@
 ---@created 16MAY22
 
 do
-
     _MSF = {}
     local soft = arg[1]
 
     if not soft then
-
         _MSF.Directory = lfs.writedir() .. [[Scripts\MSF\]]
         _MSF.ModulesDirectory = _MSF.Directory .. [[Modules\]]
         _MSF.UserDirectory = _MSF.ModulesDirectory .. [[User\]]
@@ -59,36 +57,93 @@ do
         -- Log certifies that at least all modules loaded.
         BASE:Info('%s initialization finished.', 'MSF')
     else
-        _MSF.Directory = io.popen"cd":read'*l'
+        _MSF.Directory = io.popen"cd":read'*l' .. [[\]]
+        _MSF.ModulesDirectory = _MSF.Directory .. [[Modules\]]
+        _MSF.OptionalDirectory = _MSF.ModulesDirectory .. [[Optional\]]
+        _MSF.ConfigDirectory = _MSF.Directory .. [[Config\]]
+        print(_MSF.ConfigDirectory)
+        function _MSF.Load(File)
+            local f = loadfile(File)
 
-        function _MSF:Load(File)
-            local location = self.Directory .. File
-
-            local f, err = loadfile(location)
-
-            if not f then
-                error(err)
-            else
+            if f then
                 f()
+                return true
             end
         end
 
         -- Load Routines for external use.
-        _MSF:Load([[\Modules\Required\Routines.lua]])
+        _MSF.Load(_MSF.ModulesDirectory .. [[\Required\Routines.lua]])
+
+        local OutFile = _MSF.Load(_MSF.ConfigDirectory .. 'REPOSITORIES')
 
         if soft == 'add' then
-            local Repositories = require 'Repositories'
+            local isError = false
+            local Name = arg[2]
 
-            local RepoArgument = arg[2]
+            if Name then
+                local Repository
 
-            if RepoArgument then
-                local Repository = Repositories[RepoArgument] or RepoArgument
-                local Destination = _MSF.Directory .. [[\Modules\Optional\]] .. RepoArgument
-                local Argument = Repository .. [[ ]] .. Destination
+                if OutFile ~= nil then
+                    Repository = REPOSITORIES[Name]
+                else
+                    REPOSITORIES = {}
+                end
 
-                ROUTINES.os.exec('git clone', Argument)
+                local Argument
+                local Destination
+                local URL
+
+                if Repository then
+                    Destination = _MSF.OptionalDirectory .. Name
+                    URL = Repository
+                else
+                    URL = arg[3]
+
+                    if URL then
+                        Destination = _MSF.OptionalDirectory .. Name
+                    else
+                        isError = true
+                        print('This repository is not indexed, you must provide a URL as a third argument.')
+                    end
+                end
+
+                if not isError then
+                    Argument = URL .. [[ ]] .. Destination
+
+                    ROUTINES.os.exec('git clone', Argument)
+
+                    REPOSITORIES[Name] = URL
+
+                    ROUTINES.file.EDSerializeToFile(_MSF.ConfigDirectory, 'REPOSITORIES', REPOSITORIES)
+                end
             else
-                error('You must provide a repository to add.')
+                print('You must provide a repository to add.')
+            end
+        elseif soft == 'update' then
+            if OutFile then
+                local method = arg[2]
+
+                -- Update all repositories.
+                if method == '-a' then
+                    for repo, _ in pairs(REPOSITORIES) do
+                        local path = _MSF.OptionalDirectory .. repo
+
+                        ROUTINES.os.exec(string.format('git -C %s pull origin main', path))
+                    end
+                -- Update an individual repository.
+                elseif method == '-i' then
+                    local repo = arg[3]
+
+                    if REPOSITORIES[repo] then
+                        local path = _MSF.OptionalDirectory .. repo
+
+                        ROUTINES.os.exec(string.format('git -C %s pull origin main', path))
+                    else
+                        print('Repository not indexed, cannot update.')
+                    end
+                end
+            else
+                print('Nothing to update.')
             end
         end
     end
