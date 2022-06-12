0d09051df6ed7a72fa1fd7c7606dfbba0b508128
nicelym <nicely.max@gmail.com>
2022-06-11 21:24:07 -0400

    Modules loads repositories

--- Modules.lua
+++ Modules.lua
@@ -1,5 +1,6 @@
 -- Required Modules, do not touch. Order is important.
 _MSF:Load('Config.lua', 'Config')
+_MSF:Load('REPOSITORIES', 'Config')
 _MSF:Load('Enums.lua', 'Required')
 _MSF:Load('Routines.lua', 'Required')
 _MSF:Load('Base.lua', 'Required')
