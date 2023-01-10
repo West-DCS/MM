ba774b9da3d1b2bf80125a04cf19fd39a2ca1bc8
nicelym <nicely.max@gmail.com>
2023-01-10 06:08:58 -0500

    Init Commit

--- MSFGameGUI.lua
+++ MSFGameGUI.lua
@@ -1,4 +1,26 @@
-do
-    net.log('MSFGameGUI Initialized.')
 
-end
+    _NMSF = {}
+    _NMSF.Directory = lfs.writedir() .. [[Scripts\MSF\]]
+
+    _NMSF.ModulesDirectory = _NMSF.Directory .. [[Modules\]]
+    _NMSF.RequiredDirectory = _NMSF.ModulesDirectory .. [[Required\]]
+    _NMSF.UserDirectory = _NMSF.ModulesDirectory .. [[User\]]
+    _NMSF.UserHooksDirectory = _NMSF.UserDirectory .. [[Hooks\]]
+    _NMSF.ConfigDirectory = _NMSF.Directory .. [[Config\]]
+    _NMSF.HooksDirectory = _NMSF.ModulesDirectory .. [[Hooks\]]
+
+    dofile(_NMSF.ConfigDirectory .. 'Config.lua')
+    dofile(_NMSF.RequiredDirectory .. 'Routines.lua')
+    dofile(_NMSF.HooksDirectory .. 'Base.lua')
+    dofile(_NMSF.HooksDirectory .. 'Events.lua')
+    dofile(_NMSF.RequiredDirectory .. 'Net.lua')
+
+    -- Load User Modules (non-recursively, unordered)
+    for file in lfs.dir(_NMSF.UserDirectory) do
+        if ROUTINES.file.isFile(_NMSF.UserHooksDirectory .. file) then
+            NET:Log('Loading: %s', file)
+            dofile(_NMSF.UserHooksDirectory .. file)
+        end
+    end
+
+    NET:Log('MSFGameGUI Initialized.')
