b8610b835112816a04054c3946fad57ce32bd713
Max Nicely <nicely.max@gmail.com>
2023-01-27 11:18:20 -0500

    Added TCP instatiation and methods

--- Modules/Required/Server.lua
+++ Modules/Required/Server.lua
@@ -15,6 +15,18 @@ function SERVER:New(Port)
     self.udp = assert(self.Socket.udp())
     self.udp:settimeout(0)
     self.udp:setsockname('*', self.Port)
+
+    self.Running = true
+
+    return self
+end
+
+function SERVER:NewTCP(Port)
+    local self = BASE:Inherit(self, BASE:New())
+
+    self.Socket = require('socket')
+    self.Port = Port
+
     self.Running = true
 
     return self
@@ -40,6 +52,52 @@ function SERVER:Start(Object, Callback)
     self:ScheduleRepeat(2/1000, coroutine.resume, self.co, self)
 end
 
+function SERVER:StartTCP(Object, Callback)
+    self.Server = self.Socket.tcp()
+    self.Server:settimeout(0)
+    self.Server:bind("*", self.Port)
+    self.Server:listen()
+
+    self.co = coroutine.create(function()
+        while self.Running do
+            local Client = self.Server:accept()
+
+            if Client then
+                self:Info('Client Detected.')
+
+                local Data, Error = Client:receive()
+
+                if not Error then
+                    self:L{Data}
+                    --Data = NET:JSON2LUA(Data)
+
+                    while true do
+                        local Sent = Client:send('Accepted\n')
+
+                        if Sent then break end
+
+                        coroutine.yield()
+                    end
+
+                    --Callback(Object, Data)
+                end
+
+                Client:close()
+            else
+                self:Info('No Client Detected.')
+                coroutine.yield()
+            end
+        end
+    end)
+
+    coroutine.resume(self.co, self)
+
+    self.Scheduler = self:ScheduleRepeat(0.02, coroutine.resume, self.co, self)
+end
+
 function SERVER:Stop()
     self.Running = false
+    self:ScheduleStop(self.Scheduler)
 end
+
+TCP = SERVER:NewTCP(101):StartTCP(TCP, function(Data) self:L(Data) end)
