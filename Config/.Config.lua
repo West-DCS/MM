685b4dc9a939e4e2cdb1557dac717cea644d8d6c
nicelym <nicely.max@gmail.com>
2023-01-27 12:04:16 -0500

    Removed Config.lua, but added example version

---
+++ Config/.Config.lua
@@ -0,0 +1,6 @@
+-- Example Config File
+
+CONFIG = {
+    -- Set the directory of your DCS Saved Games folder. You must include \\ at the end to escape the \
+    SavedGames = 'C:\\Users\\nicel\\Saved Games\\DCS.openbeta\\'
+}
--- Config/Config.lua
+++
@@ -1,8 +0,0 @@
----@author West#9009
----@description Keep User Config settings in here.
----@created 23MAY22
-
-CONFIG = {
-    SavedGames = 'C:\\Users\\nicel\\Saved Games\\DCS.openbeta\\'
-}
-
