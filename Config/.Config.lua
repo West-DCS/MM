b277b10fd14796ab2baa6e32972a68a440c38af5
nicelym <nicely.max@gmail.com>
2023-01-27 20:53:13 -0500

    Added Project Name example

--- Config/.Config.lua
+++ Config/.Config.lua
@@ -2,5 +2,7 @@
 
 CONFIG = {
     -- Set the directory of your DCS Saved Games folder. You must include \\ at the end to escape the \
-    SavedGames = 'C:\\Users\\nicel\\Saved Games\\DCS.openbeta\\'
+    SavedGames = 'C:\\Users\\nicel\\Saved Games\\DCS.openbeta\\',
+    -- Set the project name to the same name as your repository folder.
+    ProjectName = 'MSF'
 }
