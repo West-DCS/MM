8f2dc47a670253f3bf9497c0934e6422c8e1d879
nicelym <nicely.max@gmail.com>
2023-01-27 21:12:58 -0500

    Added comment about deleting .gitignore

--- Config/.Config.lua
+++ Config/.Config.lua
@@ -1,5 +1,5 @@
 -- Example Config File
-
+-- You can delete the .gitignore file if you want to track your Config File!
 CONFIG = {
     -- Set the directory of your DCS Saved Games folder. You must include \\ at the end to escape the \
     SavedGames = 'C:\\Users\\nicel\\Saved Games\\DCS.openbeta\\',
