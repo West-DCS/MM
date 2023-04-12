2693d9901a09f6771d8a5ecec6c2909b6891a6f7
West <nicely.max@gmail.com>
2023-04-12 10:00:32 -0400

    Made some adjustments to .Config settings to automatically find any user's Saved Games dir.

--- Config/.Config.lua
+++ Config/.Config.lua
@@ -1,10 +1,11 @@
 CONFIG = {
     -- Set the directory of your DCS Saved Games folder. You must include \\ at the end to escape the \
-    SavedGames = 'C:\\Users\\nicel\\Saved Games\\DCS.openbeta\\',
+    -- This default may work just fine for your install.
+    SavedGames = string.format('C:\\Users\\%s\\Saved Games\\DCS.openbeta\\', os.getenv('USERNAME')),
     -- Set the project name to the same name as your repository folder.
     ProjectName = 'MSF',
-    -- This is the file where information about repositories is stored.
-    Repository = {User = 'nicelym', Repo = 'MSF_Repository', Path = 'REPOSITORIES'},
+    -- This is the file where information about repositories is stored on GitHub.
+    Repository = {User = 'nicelym', Repo = 'MSF_Repositories', Path = 'REPOSITORIES'},
     -- Remote repository for remote GitHub static Lua file.
-    Remote = {User = 'nicelym', Repo = 'MSF', Path = 'Config/.Config.lua'}
+    Remote = {User = 'nicelym', Repo = 'MSF', Path = 'Builds\\{YourBuildFile}.lua'}
 }
