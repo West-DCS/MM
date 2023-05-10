5c97ab47c30516b8d47d4c0f0c67d45be32bc8c3
West <nicely.max@gmail.com>
2023-05-10 04:55:40 -0700

    Init Commit

---
+++ Modules/Commands/Listen.lua
@@ -0,0 +1,12 @@
+-- Instantiate a new COMMAND object.
+local Listen = COMMAND:New('listen', 'Listen to dcs.log.', nil)
+
+function Listen:Execute()
+    -- Open the file for reading
+    local LogFilePath = CONFIG.SavedGames .. 'Logs\\dcs.log'
+
+    -- Protect against interrupts.
+    pcall(os.execute, string.format('py %s\\Listen.py "%s"', _MSF.CommandsDirectory, LogFilePath))
+end
+
+return Listen
