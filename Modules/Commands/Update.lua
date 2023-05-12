fb58b7adf5eaebacb9a7cf8c5f11c75a48c93f7a
West <nicely.max@gmail.com>
2023-05-11 19:56:14 -0700

    Adjusted to new command functionality

--- Modules/Commands/Update.lua
+++ Modules/Commands/Update.lua
@@ -1,36 +1,41 @@
-local Update = COMMAND:New()
+local Options = {
+    -- Flag to update all modules.
+    a = {
+        fun = function(self) self.Mode = 'a' end,
+        desc = 'Update all modules.'
+    },
+    -- Flag to update a specific module
+    i = {
+        fun = function(self, Param) self.Mode = 'i'; self.Module = Param end,
+        desc = 'Update an individual module.',
+        param = 'Module'
+    }
+}
 
-function Update:Execute(Args)
-    if not Args[2] then self:Help() return end
+local Update = COMMAND:New('update', 'Update community modules.', Options)
+
+function Update:Execute()
     if not REPOSITORIES then self:Out('Nothing to update. Add a module with "add <Name>"') return end
 
-    local Flag = Args[2]
-
     -- Update all repositories.
-    if Flag == '-a' then
+    if self.Mode == 'a' then
         for Repo, _ in pairs(REPOSITORIES) do
             local Path = _MSF.OptionalDirectory .. Repo
 
             ROUTINES.git.update(Path)
         end
     -- Update an individual repository.
-    elseif Flag == '-i' then
-        local Repo = arg[3]
+    elseif self.Mode == 'i' then
+        if not self.Module then self:Out('You must provide a module to update or use -a instead to update everything.')
+            return
+        end
 
-        if not REPOSITORIES[Repo] then self:Out('Module %s not indexed, cannot update.', Repo) return end
+        if not REPOSITORIES[self.Module] then self:Out('Module %s not indexed, cannot update.', self.Module) return end
 
-        local Path = _MSF.OptionalDirectory .. Repo
+        local Path = _MSF.OptionalDirectory .. self.Module
 
         ROUTINES.git.update(Path)
-    else
-        self:Help()
     end
-end
-
-function Update:Help()
-    self:Out('Usage: update')
-    self:Out('\t[-i] <module>\tUpdate a specific module.')
-    self:Out('\t[-a]\t\tUpdate all modules.')
 end
 
 return Update
